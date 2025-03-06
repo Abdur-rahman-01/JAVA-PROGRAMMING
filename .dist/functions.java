7ac14e109a69f22e3632eaa6ae4e66544e8b935c
Abdur Rahman Qasim <asadabdurrahmanqasim@gmail.com>
2025-03-07 01:48:38 +0530

    initial commit

---
+++ .dist/functions.java
@@ -0,0 +1,22 @@
+// class functions{
+//     public static void display(){
+
+//     System.out.println("hello world");
+//     System.out.println("hello world");
+//     System.out.println("hello world");
+    
+
+//      }
+//     public static void main(String[] args) {
+//         display();
+         
+     
+// }
+// }
+
+//functions with parameters.
+class functions{
+    //returnType name(type parameter1, type parameter2, type parameter3,....)
+    //body
+    //return value
+}  
